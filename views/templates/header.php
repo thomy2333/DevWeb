e92462ad9268fa1cecee31494a144564733cf19c
thomy2333 <thomyrb30@gmail.com>
2023-09-11 21:20:31 -0300

    modified:   public/build/css/app.css 	modified:   public/build/css/app.css.map 	modified:   src/scss/base/_mixins.scss 	new file:   src/scss/ui/header/_barra.scss 	modified:   src/scss/ui/header/_header.scss 	modified:   src/scss/ui/header/_index.scss 	new file:   src/scss/ui/header/_navegacion.scss 	modified:   views/templates/header.php

--- views/templates/header.php
+++ views/templates/header.php
@@ -19,3 +19,17 @@
         </div>
     </div>
 </header>
+
+<div class="barra">
+    <div class="barra__contenido">
+        <a href="/">
+         <h2 class="barra__logo">&#60;DevWebCamp /></h2>
+        </a>
+        <nav class="navegacion">
+            <a href="/devwebcamp" class="navegacion__enlace">Evento</a>
+            <a href="/paquetes" class="navegacion__enlace">Paquete</a>
+            <a href="/workshops-conferencia" class="navegacion__enlace">Workshops / Conferencias</a>
+            <a href="/registro" class="navegacion__enlace">Comprar Pase</a>
+        </nav>
+    </div>
+</div>
