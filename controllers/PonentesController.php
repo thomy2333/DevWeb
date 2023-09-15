a606d14fb3052ffe3a353309e5478ea419a0b5af
thomy2333 <thomyrb30@gmail.com>
2023-09-15 19:22:06 -0300

    DevWeb

--- controllers/PonentesController.php
+++ controllers/PonentesController.php
@@ -1,21 +1,96 @@
 <?php
 
 namespace Controllers;
+
+use Model\Ponente;
 use MVC\Router;
+use Intervention\Image\ImageManagerStatic as Image;
 
 class PonentesController {
     public static function index(Router $router){
+        $ponentes = Ponente::all();
+
         $router->render('admin/ponentes/index', [
-            'titulo' => 'Ponentes / Conferencistas'
+            'titulo' => 'Ponentes / Conferencistas',
+            'ponentes' => $ponentes
         ]);
     }
 
     public static function crear(Router $router){
         $alertas = [];
+        $ponente = new Ponente;
+
+        if($_SERVER['REQUEST_METHOD'] === 'POST') {
+            //leer imagen
+            if(!empty($_FILES['imagen']['tmp_name'])){
+                $carpeta_imagenes = '../public/img/speakers';
+
+                //crear la carpeta si no existe
+                if(!is_dir($carpeta_imagenes)){
+                    mkdir($carpeta_imagenes, 0755, true);
+                }
+
+                $imagen_png = Image::make($_FILES['imagen']['tmp_name'])->fit(800,800)->encode('png', 80);
+                $imagen_webp = Image::make($_FILES['imagen']['tmp_name'])->fit(800,800)->encode('webp', 80);
+
+                $nombre_imagen = md5( uniqid( rand(), true));
+
+                $_POST['imagen'] = $nombre_imagen;
+            } 
+
+            $_POST['redes'] = json_encode( $_POST['redes'], JSON_UNESCAPED_SLASHES);
+
+
+            $ponente->sincronizar($_POST);
+
+            //validar
+            $alertas = $ponente->validar();
+
+            //guardar el registro
+            if(empty($alertas)){
+                //guardar las imagenes
+                $imagen_png->save($carpeta_imagenes . '/' . $nombre_imagen . ".png");
+                $imagen_webp->save($carpeta_imagenes . '/' . $nombre_imagen . ".webp");
+
+                //guardar en la base de datos
+                $resultado = $ponente->guardar();
+
+                if($resultado){
+                    header('Location: /admin/ponentes');
+                }
+            }
+        }
         
         $router->render('admin/ponentes/crear', [
             'titulo' => 'Registrar Ponente',
-            'alertas' => $alertas
+            'alertas' => $alertas,
+            'ponente' => $ponente
+        ]);
+    }
+
+    public static function editar(Router $router){
+        $alertas = [];
+        
+        //validar id
+        $id = $_GET['id'];
+        $id = filter_var($id, FILTER_VALIDATE_INT);
+
+        if(!$id){
+            header('Location: /admin/ponentes');
+        }
+
+        //obtener el ponente a editar
+        $ponente = Ponente::find($id);
+
+        if(!$ponente){
+            header('Location: /admin/ponentes');
+        }
+
+        $router->render('admin/ponentes/editar', [
+            'titulo' => 'Actualizar Ponente',
+            'alertas' => $alertas,
+            'ponente' => $ponente
+
         ]);
     }
 }
