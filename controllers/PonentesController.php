e9f5477a5026c3671f4dbd2c482fa6120801c000
thomy2333 <thomyrb30@gmail.com>
2023-09-15 02:45:01 -0300

    modified:   controllers/PonentesController.php 	modified:   public/build/css/app.css 	modified:   public/build/css/app.css.map 	modified:   public/build/js/bundle.js.min.map 	modified:   public/build/js/bundle.min.js 	modified:   public/index.php 	new file:   src/js/tags.js 	modified:   src/scss/admin/_dashboard.scss 	modified:   src/scss/base/_mixins.scss 	modified:   src/scss/ui/formulario/_formulario.scss 	new file:   views/admin/ponentes/crear.php 	new file:   views/admin/ponentes/formulario.php 	modified:   views/admin/ponentes/index.php

--- controllers/PonentesController.php
+++ controllers/PonentesController.php
@@ -9,4 +9,13 @@ class PonentesController {
             'titulo' => 'Ponentes / Conferencistas'
         ]);
     }
+
+    public static function crear(Router $router){
+        $alertas = [];
+        
+        $router->render('admin/ponentes/crear', [
+            'titulo' => 'Registrar Ponente',
+            'alertas' => $alertas
+        ]);
+    }
 }
