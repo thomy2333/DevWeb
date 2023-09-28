ef01819a1b67649baa2617d01d6b6fe761e017f3
thomy2333 <thomyrb30@gmail.com>
2023-09-28 14:34:59 -0300

    Changes to be committed:

--- views/templates/header.php
+++ views/templates/header.php
@@ -1,8 +1,16 @@
 <header class="header">
     <div class="header__contenedor">
-        <nav class="header__navegacion">
-            <a href="/registro" class="header__enlace">Registro</a>
-            <a href="/login" class="header__enlace">Iniciar Sesión</a>
+    <nav class="header__navegacion">
+
+        <?php if(is_auth()) { ?>
+            <a href="<?php echo is_admin() ? '/admin/dashboard' : '/finalizar-registro'; ?>" class="header__enlace">Administrar</a>
+            <form method="POST" action="/logout" class="header__form">
+            <input type="submit" value="Cerrar Sesión" class="header__submit">
+            </form>
+        <?php } else { ?>
+         <a href="/registro" class="header__enlace">Registro</a>
+         <a href="/login" class="header__enlace">Iniciar Sesión</a>
+        <?php } ?>
         </nav>
 
         <div class="header__contenido">
@@ -23,13 +31,15 @@
 <div class="barra">
     <div class="barra__contenido">
         <a href="/">
-         <h2 class="barra__logo">&#60;DevWebCamp /></h2>
+            <h2 class="barra__logo">
+                &#60;DevWebCamp />
+            </h2>
         </a>
         <nav class="navegacion">
-            <a href="/devwebcamp" class="navegacion__enlace">Evento</a>
-            <a href="/paquetes" class="navegacion__enlace">Paquete</a>
-            <a href="/workshops-conferencia" class="navegacion__enlace">Workshops / Conferencias</a>
-            <a href="/registro" class="navegacion__enlace">Comprar Pase</a>
+            <a href="/devwebcamp" class="navegacion__enlace <?php echo pagina_actual('/devwebcamp') ? 'navegacion__enlace--actual' : ''; ?>">Evento</a>
+            <a href="/paquetes" class="navegacion__enlace <?php echo pagina_actual('/paquetes') ? 'navegacion__enlace--actual' : ''; ?>">Paquetes</a>
+            <a href="/workshops-conferencia" class="navegacion__enlace <?php echo pagina_actual('/workshops-conferencia') ? 'navegacion__enlace--actual' : ''; ?>">Workshops / Conferencias</a>
+            <a href="/registro" class="navegacion__enlace <?php echo pagina_actual('/registro') ? 'navegacion__enlace--actual' : ''; ?>">Comprar Pase</a>
         </nav>
     </div>
 </div>
